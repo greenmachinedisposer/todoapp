5a688694fcc0c1ffa5e2d84989850b62bc0e6ff6
Jurol James Cabaluna <greenmachinedisposer@gmail.com>
2018-03-13 23:31:40 +0800

    Finished Webpack and React

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-alert('Jurol James');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+
+var Todo = createReactClass({
+    render : function () {
+        return (
+            <h1>Hello</h1>
+        );
+    }
+});
+
+ReactDOM.render(<Todo/>,document.getElementById('todo-wrapper'));
