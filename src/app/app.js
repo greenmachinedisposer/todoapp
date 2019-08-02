35eaab1c05a8e65dc97f2430595cc34bcdd597a5
Jurol James Cabaluna <greenmachinedisposer@gmail.com>
2019-08-02 20:52:13 +0800

    Updated
    Signed-off-by: Jurol James Cabaluna <greenmachinedisposer@gmail.com>

--- src/app/app.js
+++ src/app/app.js
@@ -3,12 +3,12 @@ var ReactDOM = require('react-dom');
 var createReactClass = require('create-react-class');
 
 //Components
-var Map = require('./components/map/map.js');
+// var Map = require('./components/map/map.js');
 
 var App = createReactClass({
     render : function () {
         return (
-            <Map/>
+            <h1>Test only</h1>
         );
     }
 });
